405591a9206b04559e2cfeff6decb91989c0f4af
MacOwen <maclaneowen01@gmail.com>
2021-12-24 04:06:35 -0500

    Day 1 Problem 2

--- day1/sonar_sweep_1.cpp
+++ day1/sonar_sweep_1.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main() {
 	int previous_depth;
-	int current_depth;
+	int depth;
 
 	// Input first depth reading
 	cin >> previous_depth;
 
 	// Read all depths, incrementing when depth is greater than previous depth
-	int num_decreases = 0;
-	while (cin >> current_depth) {
-		if (current_depth > previous_depth)
-			num_decreases++;
-		previous_depth = current_depth;
+	int num_increases = 0;
+	while (cin >> depth) {
+		if (depth > previous_depth)
+			num_increases++;
+		previous_depth = depth;
 	}
 
 	// Output final number of depth increases
-	cout << num_decreases << '\n';
+	cout << num_increases<< '\n';
 }
---
+++ day1/sonar_sweep_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	queue<int> window;
+	int depth;
+	int window_depth = 0;
+	// Add first 3 depths to window
+	for (int i = 0; i < 3; i++) {
+		cin >> depth;
+		window.push(depth);
+		window_depth += depth;
+	}
+
+	// Slide window one depth reading at a time, incrementing whenever window size increases
+	int num_increases = 0;
+	int prev_window_depth = window_depth;
+	while (cin >> depth) {
+		window_depth -= window.front();
+		window_depth += depth;
+		if (window_depth > prev_window_depth) {
+			num_increases++;
+		}
+		window.pop();
+		window.push(depth);
+		prev_window_depth = window_depth;
+	}
+
+	// Output final number of window depth increases
+	cout << num_increases << '\n';
+}
